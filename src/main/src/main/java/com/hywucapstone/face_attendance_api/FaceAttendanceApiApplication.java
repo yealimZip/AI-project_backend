77f3e5403829a7cb1f5e303f0c5da604853a6a9b
yealimeee <ansdpfla729@gmail.com>
2026-01-28 20:23:32 +0900

    Create FaceAttendanceApiApplication.java

---
+++ src/main/src/main/java/com/hywucapstone/face_attendance_api/FaceAttendanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.hywucapstone.face_attendance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceAttendanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceAttendanceApiApplication.class, args);
+	}
+
+}
